1d7a8498f89082f0e63261798ba771979a24b5e8
amberstyers <amberstyers@gmail.com>
2019-12-09 18:29:10 -0600

    sql connection

--- sql/connection.js
+++ sql/connection.js
@@ -7,9 +7,9 @@ class Connection {
 
       const config = {
         connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
+        host: '35.224.111.64',
+        user: 'Amber',
+        password: 'Lavg3s5h22#',
         database: 'admin'
       }
 
