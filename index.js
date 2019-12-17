8d52cddc5e69464baa5ad86cc90a2492836f617a
Sam <Sam@Macintosh.local>
2019-12-16 19:17:48 -0600

    updated index.js text

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/users', usersRouter)
 app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
-  res.send('Welcome to our server!')
+  res.send('Welcome to our updated server!')
 })
 
 app.listen(port, () => {
